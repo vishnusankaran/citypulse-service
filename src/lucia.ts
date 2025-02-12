ceb763d8c446abd95d8b88ddcb911a7d147c918c
buzooka <bio.local.tech@gmail.com>
2025-02-12 13:26:28 +0530

    initial

---
+++ src/lucia.ts
@@ -0,0 +1,20 @@
+import { lucia } from "lucia";
+import { hono } from "lucia/middleware";
+import { pg } from "@lucia-auth/adapter-postgresql";
+import postgres from "pg";
+
+const pool = new postgres.Pool({
+  connectionString: CONNECTION_URL || "",
+});
+
+export const auth = lucia({
+  env: "DEV", // "PROD" if deployed to HTTPS
+  middleware: hono(),
+  adapter: pg(pool, {
+    user: "auth_user",
+    key: "user_key",
+    session: "user_session",
+  }),
+});
+
+export type Auth = typeof auth;
