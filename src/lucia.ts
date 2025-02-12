c26a2aa3283afa39b1c790148d048c2ba38362c6
buzooka <bio.local.tech@gmail.com>
2025-02-12 13:50:51 +0530

    Fixes

--- src/lucia.ts
+++ src/lucia.ts
@@ -2,6 +2,7 @@ import { lucia } from "lucia";
 import { hono } from "lucia/middleware";
 import { pg } from "@lucia-auth/adapter-postgresql";
 import postgres from "pg";
+import "lucia/polyfill/node";
 
 const pool = new postgres.Pool({
   connectionString: POSTGRES_DATABASE_URL || "",
