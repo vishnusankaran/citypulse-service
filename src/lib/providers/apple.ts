b43b748306091f30b1b461cf381ef94a53091c07
Buzooka Deploy CI <bio.local.tech@gmail.com>
2025-08-15 03:10:19 +0530

    Changes

--- src/lib/providers/apple.ts
+++ src/lib/providers/apple.ts
@@ -1,9 +1,7 @@
 import { Apple } from 'arctic';
 import * as encoding from '@oslojs/encoding';
 
-const pemCertificate = `-----BEGIN PRIVATE KEY-----
-TmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXANCk5ldmVyIGdvbm5hIGxldCB5b3UgZG93bg0KTmV2ZXIgZ29ubmEgcnVuIGFyb3VuZCBhbmQgZGVzZXJ0IHlvdQ0KTmV2ZXIgZ29ubmEgbWFrZSB5b3UgY3J5DQpOZXZlciBnb25uYSBzYXkgZ29vZGJ5ZQ0KTmV2ZXIgZ29ubmEgdGVsbCBhIGxpZSBhbmQgaHVydCB5b3U
------END PRIVATE KEY-----`;
+const pemCertificate = '';
 const pkcs8PrivateKey = encoding.decodeBase64IgnorePadding(
   pemCertificate
     .replace('-----BEGIN PRIVATE KEY-----', '')
